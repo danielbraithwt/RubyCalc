05b6b2ac170195ab618b6be3a4aa547c58f98fc4
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-09-19 15:47:32 +1200

    Continued On Expression Builder Continued writing the expression builder class

--- init.rb
+++ init.rb
@@ -2,22 +2,25 @@ APP_ROOT = File.dirname(__FILE__)
 
 $:.unshift( File.join(APP_ROOT, 'lib') )
 require 'calculator'
+require 'expression_builder'
 
 def get_expression
-	print "Enter an expression (quit to exit): "
+	print "Enter a line to add to the expression (quit to exit): "
 	input = gets.chomp.strip
 
 	return input
 end
 
-calculator = Calculator.new
+eb = ExpressionBuilder.new
 
 puts "Welcome To RubyCalc!\n"
 
 expression = get_expression
 
 while expression != "quit" do
-	puts "The reuslt is :#{Calculator.evaluate(expression)}\n\n"
+	#puts "The reuslt is :#{Calculator.evaluate(expression)}\n\n"
+	#expression = get_expression
+	eb.add_line(expression)
 	expression = get_expression
 end
 
--- lib/expression_builder.rb
+++ lib/expression_builder.rb
@@ -8,10 +8,36 @@ class ExpressionBuilder
 		if line.index("=")
 			assignment = line.split("=")[0].chomp
 		end
+		
+		# Verify that if varaibles are used in the line they are correct
+		verify_line((line.index("=") || 0), line.length)
 
-		if assignment == "ans" && @defined_variables.includes?("ans")
+		# Add the lines assigne to the list of defined variables
+		if assignment == "ans" && !@defined_variables.includes?("ans")
 			@definied_variables << assignment
-		elsif
-
+		elsif !@defined_variables(assignment)
+			@defined_variables << assignment
+		end
+		
+		# Add the line to the list of lines
+		lines << line
 	end
+
+	def variable_defined?(var)
+		return @defined_variables.include?(var)
+	end
+
+	private
+
+	def verify_line(line)
+		# Split the line into the possible variables
+		components = line.split(/[:digit:+:punct:+]/)
+		
+		# Make sure that all the variables have been defined
+		components.each do |var|
+			raise "Variable %{var} not defined yet!"
+		end
+	end
+
+
 end
